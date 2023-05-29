015d3d083c2da40a88d731b72cfc114e8b100e64
fazemodz <45793524+fazemodz@users.noreply.github.com>
2023-05-29 16:35:18 +0100

    test2

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,6 @@ import './assets/css/bootstrap.min.css';
 import './assets/css/glightbox.min.css';
 import './assets/css/swiper-bundle.min.css';
 import './assets/css/style.css';
-import './assets/js/glightbox.min.js';
-import './assets/js/swiper-bundle.min.js';
-import './assets/js/main.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
